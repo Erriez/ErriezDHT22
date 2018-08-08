145e6f7eef3dbe4d2060fe43e6df5703645e07fe
Travis CI <travis@travis-ci.org>
2018-08-08 18:19:36 +0000

    Deploy code docs to GitHub Pages Travis build: 33
    Commit: 89de82b5b2250390e14ba4eb53d67875b49826f9

--- search/pages_0.js
+++ search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['dht22_20_2d_20am2303_20temperature_20and_20humidity_20sensor_20library_20for_20arduino',['DHT22 - AM2303 temperature and humidity sensor library for Arduino',['../index.html',1,'']]]
+  ['dht22_20_2d_20am2302_2fam2303_20temperature_20and_20humidity_20sensor_20library_20for_20arduino',['DHT22 - AM2302/AM2303 temperature and humidity sensor library for Arduino',['../index.html',1,'']]]
 ];
