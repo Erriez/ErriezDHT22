063ccb8e895c54cf165d67a6fb4a63f27b95608b
Erriez <Erriez@users.noreply.github.com>
2018-09-15 22:35:44 +0200

    Fix build scripts

--- platformio/platformio_build.bat
+++ platformio/platformio_build.bat
@@ -10,8 +10,8 @@ rem git fetch
 rem git pull
 
 rem Build example(s)
-platformio ci --lib=".." --project-conf=platformio.ini ../examples/DHT22/DHT22.ino
-platformio ci --lib=".." --project-conf=platformio.ini ../examples/DHT22Average/DHT22Average.ino
-platformio ci --lib=".." --project-conf=platformio.ini ../examples/DHT22DurationTest/DHT22DurationTest.ino
+platformio ci --lib=".." --board uno --board micro --board d1_mini --board nanoatmega328 --board pro16MHzatmega328 --board pro8MHzatmega328 --board leonardo --board megaatmega2560 --board due --board d1_mini --board nodemcuv2 --board lolin_d32 ../examples/DHT22/DHT22.ino
+platformio ci --lib=".." --board uno --board micro --board d1_mini --board nanoatmega328 --board pro16MHzatmega328 --board pro8MHzatmega328 --board leonardo --board megaatmega2560 --board due --board d1_mini --board nodemcuv2 --board lolin_d32 ../examples/DHT22Average/DHT22Average.ino
+platformio ci --lib=".." --board uno --board micro --board d1_mini --board nanoatmega328 --board pro16MHzatmega328 --board pro8MHzatmega328 --board leonardo --board megaatmega2560 --board d1_mini --board nodemcuv2 --board lolin_d32 ../examples/DHT22DurationTest/DHT22DurationTest.ino
 
 @pause
