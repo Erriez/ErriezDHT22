ee54c7a966f8f47cb1d2cd8135e123d816daf3e3
Erriez <Erriez@users.noreply.github.com>
2018-11-08 21:32:56 +0100

    Add low power example to auto build scripts

--- platformio/platformio_build.bat
+++ platformio/platformio_build.bat
@@ -13,5 +13,6 @@ rem Build example(s)
 platformio ci --lib=".." --board uno --board micro --board d1_mini --board nanoatmega328 --board pro16MHzatmega328 --board pro8MHzatmega328 --board leonardo --board megaatmega2560 --board due --board d1_mini --board nodemcuv2 --board lolin_d32 ../examples/DHT22/DHT22.ino
 platformio ci --lib=".." --board uno --board micro --board d1_mini --board nanoatmega328 --board pro16MHzatmega328 --board pro8MHzatmega328 --board leonardo --board megaatmega2560 --board due --board d1_mini --board nodemcuv2 --board lolin_d32 ../examples/DHT22Average/DHT22Average.ino
 platformio ci --lib=".." --board uno --board micro --board d1_mini --board nanoatmega328 --board pro16MHzatmega328 --board pro8MHzatmega328 --board leonardo --board megaatmega2560 --board d1_mini --board nodemcuv2 --board lolin_d32 ../examples/DHT22DurationTest/DHT22DurationTest.ino
+platformio ci --lib=".." --board uno --board micro --board d1_mini --board nanoatmega328 --board pro16MHzatmega328 --board pro8MHzatmega328 --board leonardo ../examples/DHT22LowPower/DHT22LowPower.ino
 
 @pause
