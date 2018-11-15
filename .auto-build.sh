bbe5d6117a20dcfbf3769766d77a8e0b21dc5738
Erriez <Erriez@users.noreply.github.com>
2018-11-15 21:17:51 +0100

    Add LowPower library to build script

--- .auto-build.sh
+++ .auto-build.sh
@@ -16,6 +16,9 @@ echo 'Setting up the script...'
 # Exit with nonzero exit code if anything fails
 set -e
 
+rem Install library dependency LowPower.h for example DHT22LowPower.ino
+platformio lib install id=38
+
 # Build sources
 platformio ci --lib="." --board uno --board micro --board d1_mini --board nanoatmega328 --board pro16MHzatmega328 --board pro8MHzatmega328 --board leonardo --board megaatmega2560 --board due --board d1_mini --board nodemcuv2 --board lolin_d32 examples/DHT22/DHT22.ino
 platformio ci --lib="." --board uno --board micro --board d1_mini --board nanoatmega328 --board pro16MHzatmega328 --board pro8MHzatmega328 --board leonardo --board megaatmega2560 --board due --board d1_mini --board nodemcuv2 --board lolin_d32 examples/DHT22Average/DHT22Average.ino
