b89fea5252c15fb74e0d3b38ff1a0385d9dc5240
Erriez <erriez@users.noreply.github.com>
2020-04-09 20:30:06 +0200

    Update .auto-build scripts

--- .auto-build.sh
+++ .auto-build.sh
@@ -1,26 +1,54 @@
-#!/bin/sh
+#/bin/bash
 
-################################################################################
-# Title         : .auto-build.sh
-# Date created  : 2 August 2018
-__AUTHOR__="Erriez"
-#
-# This script will start PlatformIO build.
-#
-################################################################################
-
-################################################################################
-##### Setup this script and get the current gh-pages branch.               #####
-echo 'Setting up the script...'
-
-# Exit with nonzero exit code if anything fails
+# Exit immediately if a command exits with a non-zero status.
 set -e
 
-# Install library dependency LowPower.h for example DHT22LowPower.ino
-platformio lib install id=38
+echo "Starting auto-build script..."
 
-# Build sources
-platformio ci --lib="." --board uno --board micro --board d1_mini --board nanoatmega328 --board pro16MHzatmega328 --board pro8MHzatmega328 --board leonardo --board megaatmega2560 --board due --board d1_mini --board nodemcuv2 --board lolin_d32 examples/DHT22/DHT22.ino
-platformio ci --lib="." --board uno --board micro --board d1_mini --board nanoatmega328 --board pro16MHzatmega328 --board pro8MHzatmega328 --board leonardo --board megaatmega2560 --board due --board d1_mini --board nodemcuv2 --board lolin_d32 examples/DHT22Average/DHT22Average.ino
-platformio ci --lib="." --board uno --board micro --board d1_mini --board nanoatmega328 --board pro16MHzatmega328 --board pro8MHzatmega328 --board leonardo --board megaatmega2560 --board d1_mini --board nodemcuv2 --board lolin_d32 examples/DHT22DurationTest/DHT22DurationTest.ino
-platformio ci --lib="." --board uno --board micro --board nanoatmega328 --board pro16MHzatmega328 --board pro8MHzatmega328 --board leonardo examples/DHT22LowPower/DHT22LowPower.ino
+
+function autobuild()
+{
+    # Set environment variables
+    BOARDS_AVR="--board uno --board micro --board miniatmega328 --board nanoatmega328new --board pro16MHzatmega328 --board pro8MHzatmega328 --board megaatmega2560 --board leonardo"
+    BOARDS_ARM="--board due"
+    BOARDS_ESP="--board d1_mini --board nodemcuv2 --board lolin_d32"
+
+    echo "Installing library dependencies"
+    # Install library dependency LowPower.h for example DHT22LowPower.ino
+    platformio lib --global install "Low-Power"
+
+    echo "Building examples..."
+    platformio ci --lib="." ${BOARDS_AVR} ${BOARDS_ARM} ${BOARDS_ESP} examples/DHT22/DHT22.ino
+    platformio ci --lib="." ${BOARDS_AVR} ${BOARDS_ARM} ${BOARDS_ESP} examples/DHT22Average/DHT22Average.ino
+    platformio ci --lib="." ${BOARDS_AVR} `# DUE NOT SUPPORTED` ${BOARDS_ESP} examples/DHT22DurationTest/DHT22DurationTest.ino
+    platformio ci --lib="." ${BOARDS_AVR} `# DUE/ESP NOT SUPPORTED` examples/DHT22LowPower/DHT22LowPower.ino
+}
+
+function generate_doxygen()
+{
+    echo "Generate Doxygen HTML..."
+
+    DOXYGEN_PDF="ErriezDHT22.pdf"
+
+    # Cleanup
+    rm -rf html latex
+
+    # Generate Doxygen HTML and Latex
+    doxygen Doxyfile
+
+    # Allow filenames starting with an underscore    
+    echo "" > html/.nojekyll
+
+    # Generate PDF when script is not running on Travis-CI
+    if [[ -z ${TRAVIS_BUILD_DIR} ]]; then
+        # Generate Doxygen PDF
+        make -C latex
+
+        # Copy PDF to root directory
+        cp latex/refman.pdf ./${DOXYGEN_PDF}
+    fi
+}
+
+autobuild
+generate_doxygen
+
